2e41c0fbd68a181caf6c5562235ec08020ede94e
@AnJihun <jh.ahn@infovine.co.kr>
2025-07-15 18:58:05 +0900

    작업중

--- SwiftUIFireBase/FireStore/UserManager.swift
+++ SwiftUIFireBase/FireStore/UserManager.swift
@@ -4,12 +4,28 @@
 //
 //  Created by Jihoon on 7/4/25.
 //
+import Foundation
+import FirebaseFirestore
 
 class UserManager {
+    
     static var shared = UserManager()
     
     private init() { }
     
-    
+    func createNewUser(auth: AuthDataResultModel) async throws {
+        var userData: [String: Any] = [
+            "user_id" : auth.uid,
+            "date_created" : Timestamp(),
+        ]
+        if let email = auth.email {
+            userData["email"] = email
+        }
+        if let photoURL = auth.photoURL {
+            userData["photo_url"] = photoURL
+        }
+        
+        try await Firestore.firestore().collection("users").document(auth.uid).setData(userData, merge: false)
+    }
     
 }
