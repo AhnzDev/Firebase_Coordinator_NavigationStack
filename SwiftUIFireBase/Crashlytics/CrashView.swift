199c2096fa1eb3368429b45837b5079a5ece30bd
Ahnz <AhnzDev@gmail.com>
2026-01-04 12:38:38 +0900

    - crashlytics 완료

--- SwiftUIFireBase/Crashlytics/CrashView.swift
+++ SwiftUIFireBase/Crashlytics/CrashView.swift
@@ -1,8 +1,8 @@
 //
 //  CrashView.swift
-//  SwiftUIFireBase
+//  SwiftfulFirebaseBootcamp
 //
-//  Created by Jihoon on 12/31/25.
+//  Created by Nick Sarno on 1/24/23.
 //
 
 import SwiftUI
@@ -10,21 +10,48 @@ import SwiftUI
 struct CrashView: View {
     var body: some View {
         ZStack {
-            Color.gray.ignoresSafeArea()
+            Color.gray.opacity(0.3).ignoresSafeArea()
             
-            VStack {
+            VStack(spacing: 40) {
+                
                 Button("Click me 1") {
+                    CrashManager.shared.addLog(message: "button_1_clicked")
+
                     let myString: String? = nil
-                    let string2 = myString!
+                    
+                    guard let myString else {
+                        CrashManager.shared.sendNonFatal(error: URLError(.dataNotAllowed))
+                        return
+                    }
+                    
+                    let string2 = myString
                 }
+                
                 Button("Click me 2") {
-                    fatalError("Crash was triggered")
+                    CrashManager.shared.addLog(message: "button_2_clicked")
+
+                    fatalError("This was a fatal crash.")
+                }
+                
+                Button("Click me 3") {
+                    CrashManager.shared.addLog(message: "button_3_clicked")
+
+                    let array: [String] = []
+                    let item = array[0]
                 }
             }
+        }
+        .onAppear {
+            CrashManager.shared.setUserId(userId: "ABC123")
+            CrashManager.shared.setIsPremiumValue(isPremium: false)
+            CrashManager.shared.addLog(message: "crash_view_appeared")
+            CrashManager.shared.addLog(message: "Crash view appeared on user's screen.")
         }
     }
 }
 
-#Preview {
-    CrashView()
+struct CrashView_Previews: PreviewProvider {
+    static var previews: some View {
+        CrashView()
+    }
 }
